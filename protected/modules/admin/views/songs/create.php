5a3354c61d2b7a4a4b18420519902e2871915961
priyranjansingh <singh.priyranjan@gmail.com>
2016-04-15 12:37:48 +0530

    Update create.php

--- protected/modules/admin/views/songs/create.php
+++ protected/modules/admin/views/songs/create.php
@@ -1,18 +1,23 @@
-<?php
-/* @var $this SongsController */
-/* @var $model Songs */
-
-$this->breadcrumbs=array(
-	'Songs'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Songs', 'url'=>array('index')),
-	array('label'=>'Manage Songs', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create Songs</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+  <h1>
+    Add
+    <small>Songs</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+    <li><a href="<?php echo base_url() . '/admin/songs'; ?>"><i class="fa fa-dashboard"></i> Songs</a></li>
+    <li class="active">Add</li>
+</ol>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box box-info">
+				<div class="box-header with-border">
+					<h3 class="box-title">Song</h3>
+				</div>
+				<?php $this->renderPartial('_form', array('model'=>$model,'genres'=>$genres)); ?>
+			</div>
+		</div>
+	</div>
+</section>
