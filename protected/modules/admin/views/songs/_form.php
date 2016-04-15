6aed82414d161a9102f5ef14de09aeb093db4264
priyranjansingh <singh.priyranjan@gmail.com>
2016-04-15 12:52:01 +0530

    Update _form.php

--- protected/modules/admin/views/songs/_form.php
+++ protected/modules/admin/views/songs/_form.php
@@ -1,148 +1,73 @@
-<?php
-/* @var $this SongsController */
-/* @var $model Songs */
-/* @var $form CActiveForm */
-?>
-
-<div class="form">
-
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'songs-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
+	'htmlOptions'=>array('enctype'=>'multipart/form-data'),
 )); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'id'); ?>
-		<?php echo $form->textField($model,'id',array('size'=>36,'maxlength'=>36)); ?>
-		<?php echo $form->error($model,'id'); ?>
+<div class="box-body">
+	<div class="form-group">
+		<div class="col-xs-6">
+			<?php echo $form->labelEx($model,'song_name'); ?>
+			<?php echo $form->textField($model,'song_name',array('size'=>'60','maxlength'=>'128','class' => 'form-control')); ?>
+			<?php echo $form->error($model,'song_name'); ?>
+		</div>
+		<div class="col-xs-6">
+			<?php echo $form->labelEx($model,'artist_name'); ?>
+			<?php echo $form->textField($model,'artist_name',array('size'=>'60','maxlength'=>'128','class' => 'form-control')); ?>
+			<?php echo $form->error($model,'artist_name'); ?>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'song_name'); ?>
-		<?php echo $form->textField($model,'song_name',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'song_name'); ?>
+	<div class="form-group">
+		<div class="col-xs-6">
+			<?php echo $form->labelEx($model,'s3_bucket'); ?>
+			<?php echo $form->dropDownList($model,'s3_bucket',[],array('empty'=>'Select S3 Bucket','class' => 'form-control')); ?>
+			<?php echo $form->error($model,'s3_bucket'); ?>
+		</div>
+		<div class="col-xs-6">
+			<?php echo $form->labelEx($model,'file_name'); ?>
+			<?php echo $form->fileField($model,'file_name',array('class' => 'form-control')); ?>
+			<?php echo $form->error($model,'file_name'); ?>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'artist_name'); ?>
-		<?php echo $form->textField($model,'artist_name',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'artist_name'); ?>
+	<div class="form-group">
+		<div class="col-xs-4">
+			<?php echo $form->labelEx($model,'genre'); ?>
+			<?php echo $form->dropDownList($model,'genre',$genres,
+									array(
+										'empty'=>'Select Genre',
+										'class' => 'form-control',
+										'ajax' => array(
+									                'type' => 'POST',
+									                'url' => CController::createUrl('childs'),
+									                'update' => '#Songs_sub_genre',
+									                'data' => array('genre' => 'js:this.value'),
+						        				))
+							        		); ?>
+			<?php echo $form->error($model,'genre'); ?>
+		</div>
+		<div class="col-xs-4">
+			<?php echo $form->labelEx($model,'sub_genre'); ?>
+			<?php echo $form->dropDownList($model,'sub_genre',[],
+									array(
+										'empty'=>'Select Sub Genre',
+										'class' => 'form-control',
+										'ajax' => array(
+									                'type' => 'POST',
+									                'url' => CController::createUrl('childs'),
+									                'update' => '#Songs_sub_sub_genre',
+									                'data' => array('genre' => 'js:this.value'),
+						        				))
+									); ?>
+			<?php echo $form->error($model,'sub_genre'); ?>
+		</div>
+		<div class="col-xs-4">
+			<?php echo $form->labelEx($model,'sub_sub_genre'); ?>
+			<?php echo $form->dropDownList($model,'sub_sub_genre',[],array('empty'=>'Select Sub Sub Genre','class' => 'form-control')); ?>
+			<?php echo $form->error($model,'sub_sub_genre'); ?>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'slug'); ?>
-		<?php echo $form->textField($model,'slug',array('size'=>60,'maxlength'=>256)); ?>
-		<?php echo $form->error($model,'slug'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'s3_bucket'); ?>
-		<?php echo $form->textField($model,'s3_bucket',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'s3_bucket'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'file_name'); ?>
-		<?php echo $form->textField($model,'file_name',array('size'=>60,'maxlength'=>256)); ?>
-		<?php echo $form->error($model,'file_name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'type'); ?>
-		<?php echo $form->textField($model,'type'); ?>
-		<?php echo $form->error($model,'type'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'bpm'); ?>
-		<?php echo $form->textField($model,'bpm',array('size'=>16,'maxlength'=>16)); ?>
-		<?php echo $form->error($model,'bpm'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'song_key'); ?>
-		<?php echo $form->textField($model,'song_key',array('size'=>32,'maxlength'=>32)); ?>
-		<?php echo $form->error($model,'song_key'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'file_size'); ?>
-		<?php echo $form->textField($model,'file_size',array('size'=>16,'maxlength'=>16)); ?>
-		<?php echo $form->error($model,'file_size'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'genre'); ?>
-		<?php echo $form->textField($model,'genre',array('size'=>36,'maxlength'=>36)); ?>
-		<?php echo $form->error($model,'genre'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'sub_genre'); ?>
-		<?php echo $form->textField($model,'sub_genre',array('size'=>36,'maxlength'=>36)); ?>
-		<?php echo $form->error($model,'sub_genre'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'sub_sub_genre'); ?>
-		<?php echo $form->textField($model,'sub_sub_genre',array('size'=>36,'maxlength'=>36)); ?>
-		<?php echo $form->error($model,'sub_sub_genre'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'s3_url'); ?>
-		<?php echo $form->textArea($model,'s3_url',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'s3_url'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
-		<?php echo $form->error($model,'status'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'deleted'); ?>
-		<?php echo $form->textField($model,'deleted'); ?>
-		<?php echo $form->error($model,'deleted'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'created_by'); ?>
-		<?php echo $form->textField($model,'created_by',array('size'=>36,'maxlength'=>36)); ?>
-		<?php echo $form->error($model,'created_by'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'modified_by'); ?>
-		<?php echo $form->textField($model,'modified_by',array('size'=>36,'maxlength'=>36)); ?>
-		<?php echo $form->error($model,'modified_by'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'date_entered'); ?>
-		<?php echo $form->textField($model,'date_entered'); ?>
-		<?php echo $form->error($model,'date_entered'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'date_modified'); ?>
-		<?php echo $form->textField($model,'date_modified'); ?>
-		<?php echo $form->error($model,'date_modified'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
-	</div>
-
+</div>
+<div class="box-footer">
+    <?php echo CHtml::link('Back', array('/admin/songs'), array("class" => 'btn btn-info pull-right', "style" => "margin-left:10px;")); ?>
+    <?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save', array("class" => 'btn btn-info pull-right')); ?>
+</div>
 <?php $this->endWidget(); ?>
-
-</div><!-- form -->
