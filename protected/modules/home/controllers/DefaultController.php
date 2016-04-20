c326f8686e79ba1ad9f414062525d7ec3c01216a
priyranjansingh <singh.priyranjan@gmail.com>
2016-04-20 22:46:38 +0530

    home changes

--- protected/modules/home/controllers/DefaultController.php
+++ protected/modules/home/controllers/DefaultController.php
@@ -5,36 +5,9 @@ class DefaultController extends Controller {
     public $param = 'value';
 
     public function actionIndex() {
-        /*$model = new Registration;
-        if (isset($_POST['Registration'])) {
-            $model->attributes = $_POST['Registration'];
-            if ($model->validate()) {
-                $model->password = HomeModule::encrypting($model->password);
-                $model->confirm_password = $model->password;
-                $model->email = $model->email;
-
-                $role_id = getParam('front_user_role');
-                $model->role_id = $role_id;
-                $model->status = 0;
-                $model->save();
-                // mail sending
-//                $route = 'activate';
-//                $params = array('activation_key' => $model->activation_key);
-//                $url = $this->createUrl($route, $params);
-//                $url = $_SERVER['SERVER_NAME'] . $url;
-//                $to = $model->email;
-//                $subject = 'Account Activation Link';
-//                $message = ' <a href="' . $url . '">Click on the link</a>';
-//                mailsend($to, "arommatech@gmail.com", $subject, $message);
-//                $this->redirect(base_url() . '/user/success');
-                // end of mail sending
-                //Yii::app()->session['user_id'] = $model->id;
-                //Yii::app()->session['user_name'] = $model->username;
-                //$this->redirect(array("myaccount"));
-            }
-        }*/
-        // $this->render('index',array('model' => $model));
-        $this->render('index');
+        $songs =  Songs::model()->findAll(array("condition" => "status = '1' AND deleted = 0","order"=>"date_entered desc","limit"=>20));
+        $videos = Videos::model()->findAll(array("condition" => "status = '2' AND deleted = 0","order"=>"date_entered desc","limit"=>20));
+        $this->render('index',array('songs'=>$songs,'videos'=>$videos));
     }
 
     public function actionLogin()
