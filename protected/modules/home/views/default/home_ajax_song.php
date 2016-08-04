67d797332df637d9929bc47589078ad38864a23d
priyranjan <singh.priyranjan@gmail.com>
2016-08-04 18:55:04 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	protected/runtime/application.log
    #	protected/runtime/sql.log

--- protected/modules/home/views/default/home_ajax_song.php
+++ protected/modules/home/views/default/home_ajax_song.php
@@ -9,42 +9,44 @@ $baseUrl = Yii::app()->theme->baseUrl;
             ?>    
             <li>
                 <div class="i_info">
-                    <?php 
-                        $src = $baseUrl."/img/alb1.jpg";
-                        if(!empty($song->album_art)):
-                            $src = $song->album_art;
-                        endif;
+                    <?php
+                    $src = $baseUrl . "/img/alb1.jpg";
+                    if (!empty($song->album_art)):
+                        $src = $song->album_art;
+                    endif;
                     ?>
                     <img src="<?php echo $src; ?>" /> 
                     <span class="play_btn">
                         <i class="fa fa-play-circle-o" 
-                            data-song="<?php echo $song->slug; ?>" 
-                        <?php if($song->type == 1): ?>
-                            data-type="song"
-                        <?php else: ?>
-                            data-type="video"
-                        <?php endif; ?>
-                        ></i>
+                           data-song="<?php echo $song->slug; ?>" 
+                           <?php if ($song->type == 1): ?>
+                               data-type="song"
+                           <?php else: ?>
+                               data-type="video"
+                           <?php endif; ?>
+                           ></i>
                     </span>
                     <?php $this->widget('SongWidget', array("song_id" => $song->id)); ?>
                 </div>
                 <div class="i_titel">
                     <!--<div class="it_l">
-                        <?php // echo $count; ?>
+                    <?php // echo $count; ?>
                     <!--</div>-->
                     <div  class="it_m">
-                        <h5><?php echo elipsis($song->song_name, '..', 35); ?></h5>
+                        <h5>
+                            <a href="<?php echo base_url() . '/media?name=' . $song->slug; ?>"><?php echo elipsis($song->song_name, '..', 35); ?></a>
+                        </h5>
                         <h6><?php echo elipsis($song->artist_name, '..', 35); ?></h6>
                     </div>
                     <div  class="it_r"> 
                         <div class="bpm">
-                            <?php if(!empty($song->bpm)): ?>
-                            BPM: <span><?php echo $song->bpm; ?></span>
+                            <?php if (!empty($song->bpm)): ?>
+                                BPM: <span><?php echo $song->bpm; ?></span>
                             <?php endif; ?>
                         </div>
                         <div class="key">
-                            <?php if(!empty($song->song_key)): ?>
-                            KEY: <span><?php echo $song->song_key; ?></span>
+                            <?php if (!empty($song->song_key)): ?>
+                                KEY: <span><?php echo $song->song_key; ?></span>
                             <?php endif; ?>
                         </div>
                     </div>
@@ -56,16 +58,14 @@ $baseUrl = Yii::app()->theme->baseUrl;
             $count = $count + 1;
         }
     }
-    else
-    {
-    ?>    
-            <li>
-                No record found
-            </li>    
-            
-    <?php     
-    }    
-        
+    else {
+        ?>    
+        <li>
+            No record found
+        </li>    
+
+        <?php
+    }
     ?>
 </ul>
 
